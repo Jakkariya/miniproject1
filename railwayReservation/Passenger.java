50ae492b1d74d196e3db1ca6e3740846fa1227b4
JAKKARIYA ROWTHER M <140997366+Jakkariya@users.noreply.github.com>
2024-09-07 20:07:48 +0530

    Add files via upload

---
+++ railwayReservation/Passenger.java
@@ -0,0 +1,72 @@
+package railwayReservation;
+
+public class Passenger {
+	private static int idProvider=0;
+	private int id;
+	private String name;
+	private int age;
+	private char preference;
+	private String ticketType;
+	private int seatNumber;
+	
+	
+	
+	public Passenger(String name, int age, char preference) {
+		super();
+		this.id = ++idProvider;
+		this.name = name;
+		this.age = age;
+		this.preference = preference;
+	}
+	public static int getIdProvider() {
+		return idProvider;
+	}
+	public static void setIdProvider(int idProvider) {
+		Passenger.idProvider = idProvider;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getPreference() {
+		return preference;
+	}
+	public void setPreference(char preference) {
+		this.preference = preference;
+	}
+	public String getTicketType() {
+		return ticketType;
+	}
+	public void setTicketType(String ticketType) {
+		this.ticketType = ticketType;
+	}
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+	@Override
+	public String toString() {
+		return "Passenger [id=" + id + ", name=" + name + ", age=" + age + ", preference=" + preference
+				+ ", ticketType=" + ticketType + ", seatNumber=" + seatNumber + "]";
+	}
+	
+	
+	
+	
+}
